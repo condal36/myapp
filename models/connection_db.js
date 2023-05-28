0a1b6adb10ac002ed706353bdeb9e37e2e20b0f8
condal36 <r0742009@ems.niu.edu.tw>
2023-05-28 11:28:45 +0800

    connecting success

--- models/connection_db.js
+++ models/connection_db.js
@@ -12,7 +12,7 @@ const connection = mysqlt.createConnection({
 connection.connect(err => {
   if (err) {
     //console.log('host:',config.mysql.host,'user:',config.mysql.user,'password:',config.mysql.password,'database:',config.mysql.database,'connecting error');
-    console.log('connecting error');
+    console.log('connecting error',err.message);
   } else {
     console.log('connecting success');
   }
